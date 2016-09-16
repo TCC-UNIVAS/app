d520dbebc6716e7ad199d2f647d57ef2dfe8850d
Daniela Mara <danielamaradms@gmail.com>
2016-09-15 21:31:59 -0300

    Adequações do login

--- app/app.ts
+++ app/app.ts
@@ -1,19 +1,24 @@
-import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
-import {StatusBar} from 'ionic-native';
-import {TabsPage} from './pages/tabs/tabs';
-import {LoginPage} from './pages/login/login';
-
+import { Component } from '@angular/core';
+import { Platform, ionicBootstrap } from 'ionic-angular';
+import { StatusBar } from 'ionic-native';
+import { TabsPage } from './pages/tabs/tabs';
+import { LoginPage } from './pages/login/login';
+import { LoginService } from './pages/login/login.service';
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+  template: '<ion-nav [root]="rootPage"></ion-nav>',
+  providers: [LoginService],
 })
 export class MyApp {
-
   private rootPage: any;
 
-  constructor(private platform: Platform) {
-    this.rootPage = LoginPage;
+  constructor(private platform: Platform, private loginService: LoginService) {
+    let user = this.loginService.getUserFromLocalstorage();
+    if (user != undefined) {
+      this.rootPage = TabsPage;
+    } else {
+      this.rootPage = LoginPage;
+    }
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
